11ed99664953d3d272f1c0a297fce5aafed6c5fd
jnoziglia <julian.noziglia@gmail.com>
2015-06-14 15:45:29 -0300

    Creo DAOCuenta, agregarCuenta, y modificarCuenta

---
+++ src/PagoElectronico/PagoElectronico/CapaDAO/DAOCuenta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PagoElectronico.CapaDAO
+{
+    class DAOCuenta:SqlConnector
+    {
+        public static void agregarCuenta(int id_usuario, int moneda, int pais, int tipo_cuenta)
+        {
+            executeProcedure("agregarCuenta", id_usuario, moneda, pais, tipo_cuenta);
+        }
+
+        public static void modificarCuenta(int id, int id_usuario, int moneda, int pais, int tipo_cuenta)
+        {
+            executeProcedure("modificarCuenta", id, id_usuario, moneda, pais, tipo_cuenta);
+        }
+    }
+}
--- src/PagoElectronico/PagoElectronico/CapaDAO/DAORol.cs
+++ src/PagoElectronico/PagoElectronico/CapaDAO/DAORol.cs
@@ -7,5 +7,19 @@ namespace PagoElectronico.CapaDAO
 {
     class DAORol:SqlConnector
     {
+        //GET
+        public static DataTable getRoles()
+        {
+            return retrieveDataTable("getRoles");
+        }
+
+        //Existe ROL
+        public static bool existeRol(string nombre)
+        {
+            var dt = retrieveDataTable("getRolNom", nombre);
+            return dt.Rows.Count > 0;
+        }
+
+
     }
 }
