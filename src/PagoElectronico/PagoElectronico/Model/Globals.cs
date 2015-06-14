3c00e9908da27e41fff1c23ba2416b53801c96a6
jnoziglia <julian.noziglia@gmail.com>
2015-06-13 21:16:26 -0300

    Agrego clase para conexion con la base y clase global (revisar). No me toma la clase de sistema ConfigurationManager

---
+++ src/PagoElectronico/PagoElectronico/Model/Globals.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace PagoElectronico
+{
+    class Globals
+    {
+        private static int _userID = 0;
+
+        private static string connectionString = ConfigurationManager.ConnectionStrings["BaseDatos"].ConnectionString;
+        private static bool isLogged = false;
+
+        public static int userID
+        {
+            get { return _userID; }
+            set { _userID = value; }
+        }
+        public static string getConnectionString()
+        {
+            return connectionString;
+        }
+
+        public static DateTime getFechaSistema()
+        {
+            return Convert.ToDateTime(ConfigurationSettings.AppSettings["fechaSistema"]);
+        }
+
+        public static void setAdminLoggeado(bool status)
+        {
+            isLogged = status;
+        }
+
+        public static bool adminLoggeado()
+        {
+            return isLogged;
+        }
+
+
+    }
+}
